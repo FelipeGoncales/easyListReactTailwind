98a5143123fbf693e07360d90235a3e90391b10c
FelipeGoncales <felipeggoncales22@gmail.com>
2025-08-18 23:47:27 -0300

    teste

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,13 +1,12 @@
 import { useNavigate } from "react-router-dom";
 
-function FormLogin(props) {
+function FormLogin() {
 
     const navigate = useNavigate();
 
     return (
         <form
             className="flex flex-col p-6 rounded-2xl bg-slate-400 items-center justify-center gap-6 w-[90%] max-w-[350px]" 
-            onSubmit={(e) => props.onFormSubmit(e)}
         >
             <h1 className="subtitle">Faça login</h1>
 
@@ -15,16 +14,12 @@ function FormLogin(props) {
                 className="inputForm"
                 type="text" 
                 placeholder="E-mail*" 
-                value={props.email}
-                onChange={(e) => props.setEmail(e.target.value)}
             />
 
             <input
                 className="inputForm" 
                 type="password" 
                 placeholder="Senha*" 
-                value={props.senha}
-                onChange={(e) => props.setSenha(e.target.value)}
             />
 
             <div className="w-full flex flex-col items-center justify-center gap-3">
@@ -36,7 +31,7 @@ function FormLogin(props) {
                 </button>
 
                 <p className="text-[15px] text-slate-700">
-                    Não possui uma conta? <span className="text-slate-900 font-semibold cursor-pointer hover:underline" onClick={() => navigate('/cadastro')}>Cadastre-se</span>
+                    Não possui uma conta? <span className="text-slate-900 font-semibold cursor-pointer hover:underline">Cadastre-se</span>
                 </p>
             </div>
             
